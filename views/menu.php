5d394f179f48c5cfacd1506ce90a341f21be05dd
izaitere <ismahenez@gmail.com>
2017-02-25 18:55:16 -0500

    mise en place views

---
+++ views/menu.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: izaitere
+ * Date: 2017-01-17
+ * Time: 12:02
+ */
+?>
+<nav>
+        <ul>
+            <li class="<?php if($page_title== 'accueil'){
+                 echo 'active'; } ?> "  ><a href="index.php">Accueil</a></li>
+            <li><a href="catalog.php">Thèmes</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="apropos.php">A Propos</a></li>
+
+
+
+
+        </ul>
+    </nav>
+
---
+++ views/page_head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= NOM_SITE, ':',$page_title?></title>
+    <meta name="author" content="Prénom Nom" >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Mettre ici un résumé descriptif du contenu de la page avec des mots-clés" >
+    <meta name="robots" content="index,follow" >
+    <link rel="stylesheet" href="css/main.css" >
+
+
+</head>
+<body>
