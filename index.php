dd1f680db445f6d9199eaa004f93e9a1aaca31cb
izaitere <ismahenez@gmail.com>
2017-02-27 12:23:11 -0500

    slider

--- index.php
+++ index.php
@@ -9,6 +9,52 @@ require_once 'views/header.php';
 ?>
 <main>
     <div class="wrapper">
+        <section class="row col-16">
+            <section id="slider">
+                <input type="radio" name="slider" id="slide1" checked/>
+                <input type="radio" name="slider" id="slide2"/>
+                <input type="radio" name="slider" id="slide3"/>
+                <input type="radio" name="slider" id="slide4"/>
+                <input type="radio" name="slider" id="slide5"/>
+                <!--carroussel-->
+                <div id="slides">
+                    <div id="overflow">
+                        <div class="inner">
+                            <article>
+                                <div class="info"><h3>Les roses sous toutes leurs formes</h3></div>
+                                <img src="images/bouqslider16.jpg" alt="bouquet de roses multicolores">
+                            </article>
+                            <article>
+                                <div class="info"><h3>Les orchidées</h3></div>
+                                <img src="images/bouqslider10.jpg" alt="les orchidées">
+                            </article>
+                            <article>
+                                <div class="info"><h3>Le bouquet haut en couleurs</h3></div>
+                                <img src="images/bouqslider11.jpg" alt="bouquet fleuri">
+                            </article>
+                            <article>
+                                <div class="info"><h3>Exprimez votre passion</h3></div>
+                                <img src="images/bouqslider15.jpg" alt="Les roses rouge passion">
+                            </article>
+                            <article>
+                                <div class="info"><h3>Bouquet Pinky pour les plus féminines</h3></div>
+                                <img src="images/bouqslider5.jpg" alt="Bouquet de roses">
+                            </article>
+                        </div>
+                    </div>
+                </div>
+                <!--les boutons de controle-->
+                <div id="active">
+                    <label for="slide1"></label>
+                    <label for="slide2"></label>
+                    <label for="slide3"></label>
+                    <label for="slide4"></label>
+                    <label for="slide5"></label>
+                </div>
+                <!--END ACTIVE-->
+                <!--end slider-->
+            </section>
+        </section>
         <div class="row col-16">
             <aside class="col-4">
                 <h2>Thèmes</h2>
---
+++ livraison.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'defines.php';
+$page_title = 'livraison';
+?>
+
+<?php
+require_once 'views/page_head.php';
+require_once 'views/header.php';
+?>
+<main>
+    <div class="wrapper">
+
+    </div>
+</main>
+<?php
+require_once 'views/footer.php';
+?>
+</body>
+</html>
